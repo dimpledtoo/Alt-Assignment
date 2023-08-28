193e669c093a74f6cff380bf0f06cc7f0ce88809
dimpledtoo <127685339+dimpledtoo@users.noreply.github.com>
2023-08-28 20:36:58 +0000

    Assignment Solution

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,11 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
-    return sum
+    for (let i = 1; i <= destination; i++) sum += i;
+    return sum;
 }
 
-
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+ assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -39,6 +39,14 @@ function countEvenNumbersWithin(destination) {
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for (let i =1; i <= destination; i++) {
+        if (i % 2 === 0) {
+            sum += i;
+            count += 1;
+            arrayOfEvenNumbers.push(i)
+        }
+    }
+
     return {
         // property value shorthand
         // when the property name and the value name are the same
@@ -49,7 +57,7 @@ function countEvenNumbersWithin(destination) {
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+ assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
